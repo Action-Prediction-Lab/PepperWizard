8e290cae9d9ea28697382a00407193ecf86b1efa
jwgcurrie <joelcurrie3@gmail.com>
2026-01-15 11:06:30 +0100

    chore: update build script to cache T5-base model

--- pepper_wizard/utils/download_model.py
+++ pepper_wizard/utils/download_model.py
@@ -1,7 +1,11 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-model_name = "pszemraj/grammar-synthesis-small"
-print(f"Downloading model: {model_name}...")
-AutoTokenizer.from_pretrained(model_name)
-AutoModelForSeq2SeqLM.from_pretrained(model_name)
-print("Model downloaded successfully.")
+def download_model():
+    model_name = "vennify/t5-base-grammar-correction"
+    print(f"Downloading model: {model_name}...")
+    AutoTokenizer.from_pretrained(model_name)
+    AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    print("Download complete.")
+
+if __name__ == "__main__":
+    download_model()
