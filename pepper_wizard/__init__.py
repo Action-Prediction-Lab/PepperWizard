660b6869f1b98db15ed140e0a3a6aae29913ab40
jwgcurrie <joelcurrie3@gmail.com>
2026-01-25 13:39:14 +0000

    modify init

--- pepper_wizard/__init__.py
+++ pepper_wizard/__init__.py
@@ -1,3 +1,4 @@
 # This file marks this directory as a Python package.
 # It can be empty, but its presence is necessary for Python to recognise
 # 'pepper_wizard' as a package and allow modular imports.
+from .state_buffer import StateBuffer
